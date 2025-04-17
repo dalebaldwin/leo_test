5d53dd31de7f239d76fa0dafa58b54d58eb8452b
Dale Baldwin <dale@atypical.net.au>
2025-04-17 12:47:14 +1000

    feat: Moving footer code to it's own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Navigation } from '@/components/navigation/Navigation'
+import { Footer } from '@/components/footer/Footer'
 
 export default function Home() {
   return (
     <div>
       <Navigation />
       <main></main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <p>Test project by Dale Baldwin</p>
-      </footer>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import { Box, Flex, Container, Button, Text } from '@chakra-ui/react'
+
+export const Footer = () => (
+  <Container as="footer">
+    <Text>Test project by Dale Baldwin</Text>
+  </Container>
+)
