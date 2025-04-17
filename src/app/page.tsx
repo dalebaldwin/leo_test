0b81d454c99b6a1e0dec2b4022fcae39ded85b60
Dale Baldwin <dale@atypical.net.au>
2025-04-17 12:35:39 +1000

    fix: Setting some basic nav element components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from 'next/image'
+import { Navigation } from '@/components/navigation/Navigation'
 
 export default function Home() {
   return (
     <div>
+      <Navigation />
       <main></main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <p>Test project by Dale Baldwin</p>
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex, Container, Button, Text } from '@chakra-ui/react'
+
+export const Navigation = () => {
+  return (
+    <nav className="sticky">
+      <Container>
+        <Flex justify="space-between" shrink="1">
+          <Box py="4">
+            <Text fontWeight="semibold">Leonardo.Ai Web Team Technical Challenge V3.4.1</Text>
+          </Box>
+          <Box py="4">
+            <Button>Sign In</Button>
+          </Box>
+        </Flex>
+      </Container>
+    </nav>
+  )
+}
